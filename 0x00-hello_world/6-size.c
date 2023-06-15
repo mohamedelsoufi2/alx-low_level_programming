260861d8b047817cceff7ee83716c5233cd0f6c6
root <mohamedelsofy@outlook.com>
2023-06-15 04:40:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main : - A program that prints the size of varios computer types
+ * Return 0 (success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	printif("Size of a char : %lu byte(s)\n", (unsigned long) sizeof(a));
+	printif("Size of a int : %lu byte(s)\n", (unsigned long) sizeof(b));
+	printif("Size of a long int : %lu byte(s)\n", (unsigned long) sizeof(c));
+	printif("Size of a long long int : %lu byte(s)\n", (unsigned long) sizeof(d));
+	printif("Size of a float : %lu byte(s)\n", (unsigned long) sizeof(f));
+
