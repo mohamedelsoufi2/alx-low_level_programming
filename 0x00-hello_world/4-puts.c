bc5ce45f4a755bb2dc6cb1997796cd8cfee374c4
root <mohamedelsofy@outlook.com>
2023-06-15 04:02:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,6 @@
  * Return: 0 (Success)
  */
 int main(void){
-
-	puts("Programming is like building a multilingual puzzle");
-
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
