fe53205d7e7add95315e8c1070038d2aa3eff035
root <mohamedelsofy@outlook.com>
2023-06-15 04:41:01 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,4 @@ int main(void)
 	printif("Size of a long int : %lu byte(s)\n", (unsigned long) sizeof(c));
 	printif("Size of a long long int : %lu byte(s)\n", (unsigned long) sizeof(d));
 	printif("Size of a float : %lu byte(s)\n", (unsigned long) sizeof(f));
-
+}
