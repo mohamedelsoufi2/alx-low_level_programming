ad41f88ebd2207e3e0ef3085fe6c2c76f95f542c
root <mohamedelsofy@outlook.com>
2023-06-15 06:00:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 
 /**
- * main - Entry
+ * main - 101-qoute.c
+ * description - Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  * Return: 1
  */
 int main(void)
