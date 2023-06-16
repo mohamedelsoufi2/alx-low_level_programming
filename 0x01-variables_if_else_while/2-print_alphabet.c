d9de3f128a3b37dd1f31bb98c2563d8b0d30071b
root <mohamedelsofy@outlook.com>
2023-06-16 05:21:47 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints to alphabetic
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+	char 0;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
