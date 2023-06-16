58e75a145c7cbc89c13623b363652e0cb083511e
root <mohamedelsofy@outlook.com>
2023-06-16 05:38:28 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,13 @@
 
 int main(void)
 {
-	char c;
-	
-	for (c = 'a'; c <= 'z'; c++)
-	putchar(c);
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
