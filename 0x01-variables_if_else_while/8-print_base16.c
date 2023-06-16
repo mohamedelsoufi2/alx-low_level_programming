2e273dfcaf10e207638052998c16692a3f1cffbc
root <mohamedelsofy@outlook.com>
2023-06-16 06:21:39 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,9 +15,9 @@ int main(void)
 	for (d = '0'; d <= '9'; d++)
 		putchar(d);
 
-        for (low = 'a'; low <= 'f'; low++)
-                putchar(low);
-        putchar('\n');
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
