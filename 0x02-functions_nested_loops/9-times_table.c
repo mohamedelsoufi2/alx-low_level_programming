32a3ac98eca24ce1ac3dfb7eb8007f5ec572b917
root <mohamedelsofy@outlook.com>
2023-06-20 03:54:45 -0700

     9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * times_table - Prints the 9 times table, starting with 0
+ * Return: empty output
  */
 
 void times_table(void)
