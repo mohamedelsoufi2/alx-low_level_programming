a4da2107d26f40f6ab0715faab06f326ef3628a3
root <mohamedelsofy@outlook.com>
2023-06-20 04:13:24 -0700

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,35 +11,35 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				breake;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	breake;
 	}
 	else
 	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				breake;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	breake;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
 	}
 
 }
