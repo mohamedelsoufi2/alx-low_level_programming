40f5d091de796351e1471aab3653a8abb9ac1b81
root <mohamedelsofy@outlook.com>
2023-06-20 03:50:07 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_last_digit - Prints the last digit of a number
+ *@n: The number to be tread
+ * Return: Value of the last digit of number
  */
 
 int print_last_digit(int n)
